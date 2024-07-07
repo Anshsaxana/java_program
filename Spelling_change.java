344e05e47b1af1813f7789620c37261d799153f3
ansh <anshsaxena1980@gmail.com>
2024-07-07 13:04:41 +0530

    spelling correct

--- Spelling_change.java
+++ Spelling_change.java
@@ -2,9 +2,22 @@ public class Spelling_change {
     public static void main(String[] args) {
         Spelling_change m= new Spelling_change();
         String st="ansh";
-        m.replce(st, 0, 0);
+        char r='n';
+        char ch='r';
+        m.replce(st,r,ch);
+        
     } 
-    public void replce(String st ,char r,char ch){
-
+    public String replce(String st ,char r,char ch){
+        String res="";
+        for(int i=0;i<st.length();i++){
+            char chr=st.charAt(i);
+            if (chr==r) {
+                res=res+ch;
+            }else{
+                res=res+chr;
+            }
+        }
+        System.out.println(res);
+        return res;
     }
 }
