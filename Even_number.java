f590de7350b545f851347253bd06259df7745fce
Anshsaxana <anshsaxena1980@gmail.com>
2025-07-07 18:19:27 +0530

    Update Even_number.java

--- Even_number.java
+++ Even_number.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Even_number {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
+          System.out.println ("Enter your number ") ;
+        int num= sc.nextInt();
         for(int i=0;i<=100;i++){
             if (i%2==0) {
                 System.out.println(i);
